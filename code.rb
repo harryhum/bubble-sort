72e1d406414a962dc88e5c77ed6733fb3f6b3ae5
Harry Hum <harryhum22@hotmail.com>
2017-11-14 14:37:57 -0500

    Added sort by block

--- code.rb
+++ code.rb
@@ -2,20 +2,38 @@ def bubble_sort(array)
     n = array.length
     swapped = true
     while swapped
-        puts 'a'
         i = 1
         swapped = false
         while i <= n - 1
-            puts 'b'
             if array[i - 1] > array[i]
-                puts'c'
                 array[i - 1], array[i] = array[i], array[i - 1]
                 swapped = true
             end
             i += 1
         end
     end
-    puts 'e'
-    array
+    puts array.inspect
 end
 
+def bubble_sort_by(array)
+    n = array.length
+    swapped = true
+    while swapped
+        i = 1
+        swapped = false
+        while i <= n - 1
+            if yield(array[i - 1], array[i]) > 0
+                array[i - 1], array[i] = array[i], array[i - 1]
+                swapped = true
+            end
+            i += 1
+        end
+    end
+    puts array.inspect
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+    left.length - right.length
+end
